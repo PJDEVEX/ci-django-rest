9e92d5d7985481256c18c501174a0c549eea43cd
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-05 10:30:14 +0000

    Add profile detail view

--- app_profile/urls.py
+++ app_profile/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProfileList
+from .views import ProfileList, ProfileDetail
 
 urlpatterns = [
-    path('profiles/', ProfileList.as_view(), name='profile-list'),
+    path('profiles/', ProfileList.as_view(), name='profile-list'), # Listview
+    # Add detail view path
+    path('profiles/<int:pk>',ProfileDetail.as_view(), name='profile_detail') # DetailView
 ]
--- app_profile/views.py
+++ app_profile/views.py
@@ -1,13 +1,47 @@
+from django.http import Http404
+# import status to check the statatus check
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 
+
 class ProfileList(APIView):
     """
     API view to list all profiles.
     """
+
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    # Add the profile details to the view
+    serializer_class = ProfileSerializer
+    # get_object method to fetch profile by primary key
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    # 3. Implement get method to retrieve a profile by id
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    # Implement put method to update a profile by id
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
