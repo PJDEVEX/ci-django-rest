01b29e6b78231a3b5c899c8de6a9282013b2415f
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-05 11:36:59 +0000

    Add login/logout views, IsOwner permission and the is_owner to ProfileSerializer

--- app_profile/serializers.py
+++ app_profile/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from .models import Profile
 
+# Serializer for the Profile model
 class ProfileSerializer(serializers.ModelSerializer):
+    # SerializerMethodField to add an extra field 'is_owner'
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
+        # Method to determine if the current user is the owner of the profile
         request =self.context['request']
         return request.user == obj.owner
 
