e581f33bfa945bd0e70c26962505f9271944f770
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-05 19:50:06 +0000

    Add detail, reterive, update and delete post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,10 +6,10 @@ from .models import Post
 from rest_framework import status
 from .serializers import PostSerializer
 from .models import Post
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from drf.permissions import IsOwnerOrReadOnly
 
 class PostList(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
 
     def get(self, request):
@@ -30,3 +31,40 @@ class PostList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class PostDetail(APIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    # Implement get_object method to handle post not found
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    # Define the get method to retrieve a post by ID
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, context={'request': request})
+        return Response(serializer.data)
+
+    # Implement the put method to update a post by ID
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Define the delete method to delete a post by ID
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
