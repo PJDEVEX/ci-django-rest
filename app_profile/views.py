3e6375e808236d25b371fea7c570ae43d684d81a
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-06 22:02:43 +0000

    Code refact

--- app_profile/views.py
+++ app_profile/views.py
@@ -1,53 +1,22 @@
-from django.http import Http404
-# import status to check the statatus check
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from drf.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
-    API view to list all profiles.
+    List all profiles.
+    No create view as profile creation is handled by django signals.
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles,
-            many=True,
-            context={'request': request}
-            )
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    # Add the profile details to the view
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    # Allowable permission class
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-    # get_object method to fetch profile by primary key
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            # Checking the permission explicitly
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    # 3. Implement get method to retrieve a profile by id
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    # Implement put method to update a profile by id
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
--- posts/views.py
+++ posts/views.py
@@ -1,72 +1,26 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, permissions
-from .serializers import PostSerializer
-from .models import Post
+from rest_framework import generics, permissions
 from drf.permissions import IsOwnerOrReadOnly
-from rest_framework.exceptions import PermissionDenied
+from .models import Post
+from .serializers import PostSerializer
 
-class PostList(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
+
+class PostList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
 
-    def get(self, request):
-        """
-        List all posts.
-        """
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True, context={'request': request})
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        """
-        Create a new post.
-        """
-        if not request.user.is_authenticated:
-            return Response({'detail': 'Authentication credentials were not provided.'}, status=status.HTTP_403_FORBIDDEN)
 
-        serializer = PostSerializer(data=request.data, context={'request': request})
-
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-class PostDetail(APIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    # Implement get_object method to handle post not found
-    def get_object(self, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
-        except PermissionDenied:
-            raise Http403
-
-    # Define the get method to retrieve a post by ID
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, context={'request': request})
-        return Response(serializer.data)
-
-    # Implement the put method to update a post by ID
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # Define the delete method to delete a post by ID
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Post.objects.all()
