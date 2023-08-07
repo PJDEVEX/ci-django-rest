91094849ffb65c510d42cbe1d55fc919cf50c3be
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-07 19:54:29 +0000

    Add srlzr flt'er, followers and like to profiles and posts

--- app_profile/serializers.py
+++ app_profile/serializers.py
@@ -1,18 +1,31 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 # Serializer for the Profile model
 class ProfileSerializer(serializers.ModelSerializer):
     # SerializerMethodField to add an extra field 'is_owner'
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()  # Define the following_id field
 
     def get_is_owner(self, obj):
         # Method to determine if the current user is the owner of the profile
         request =self.context['request']
         return request.user == obj.owner
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(owner=user, followed=obj.owner).first()
+            return following.id if following else None
+        return None
+
 
     class Meta:
         model = Profile
         fields = ['id', 'owner', 'created_at', 'updated_at', 'name',
-        'content', 'image', 'is_owner' ]
+        'content', 'image', 'is_owner', 'following_id',  ]
+    
+    
+
