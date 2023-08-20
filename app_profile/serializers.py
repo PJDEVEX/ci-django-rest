e44b91c09d11d78d6e481d15884d15b7cd076a42
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-20 12:36:49 +0000

    Refact code

--- app_profile/serializers.py
+++ app_profile/serializers.py
@@ -3,6 +3,8 @@ from .models import Profile
 from followers.models import Follower
 
 # Serializer for the Profile model
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     # SerializerMethodField to add an extra field 'is_owner'
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -15,24 +17,23 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def get_is_owner(self, obj):
         # Method to determine if the current user is the owner of the profile
-        request =self.context['request']
+        request = self.context['request']
         return request.user == obj.owner
-    
+
     def get_following_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            following = Follower.objects.filter(owner=user, followed=obj.owner).first()
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            # print(following)
             return following.id if following else None
         return None
-
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-        'content', 'image', 'is_owner', 'following_id',
-        'posts_count', 'followers_count', 'following_count',
+            'content', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count',
         ]
-    
-    
-
