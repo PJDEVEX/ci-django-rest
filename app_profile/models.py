b9b78c3fffb8abc6064124ba919f95abafff2382
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-20 12:31:13 +0000

    Refact code

--- app_profile/models.py
+++ app_profile/models.py
@@ -10,9 +10,9 @@ class Profile(models.Model):
     """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
-    content = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/',
         default='https://res.cloudinary.com/pjdevex/image/upload/v1691082053/default_profile_gj2yan.jpg'
