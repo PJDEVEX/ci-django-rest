da8a6e0c05e4dd6b9b96b689f8b2e6ec23ef73ee
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-21 19:43:14 +0000

    Bug fix: profile detail URL path (404 error)

--- app_profile/urls.py
+++ app_profile/urls.py
@@ -4,5 +4,5 @@ from .views import ProfileList, ProfileDetail
 urlpatterns = [
     path('profiles/', ProfileList.as_view(), name='profile-list'), # Listview
     # Add detail view path
-    path('profiles/<int:pk>',ProfileDetail.as_view(), name='profile_detail') # DetailView
+    path('profiles/<int:pk>/',ProfileDetail.as_view(), name='profile_detail') # DetailView
 ]
